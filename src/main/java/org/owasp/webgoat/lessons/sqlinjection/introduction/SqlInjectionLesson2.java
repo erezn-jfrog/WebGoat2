b3ac0d629206d55b53b9c79dca05124985888686
Erez Noam <erezn@jfrog.com>
2025-01-30 14:50:44 +0200

    Update SqlInjectionLesson2.java

--- src/main/java/org/owasp/webgoat/lessons/sqlinjection/introduction/SqlInjectionLesson2.java
+++ src/main/java/org/owasp/webgoat/lessons/sqlinjection/introduction/SqlInjectionLesson2.java
@@ -62,7 +62,8 @@ public class SqlInjectionLesson2 extends AssignmentEndpoint {
   protected AttackResult injectableQuery(String query) {
     try (var connection = dataSource.getConnection()) {
       Statement statement = connection.createStatement(TYPE_SCROLL_INSENSITIVE, CONCUR_READ_ONLY);
-      ResultSet results = statement.executeQuery(query);
+      ResultSet results = null;
+     //ResultSet results = statement.executeQuery(query);
       StringBuilder output = new StringBuilder();
 
       results.first();
